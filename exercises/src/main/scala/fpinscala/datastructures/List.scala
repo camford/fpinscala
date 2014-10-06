c8c3b6a60d534bbc239ad029146c4c83e7c7d8ed
Cameron <code@cameronford.net>
2014-10-06 23:31:58 +1100

    More exercises from Chapter 3 complete

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -47,15 +47,44 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => t
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => Cons(h, t)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = {
+    def loop(l: List[A], n: Int): List[A] = {
+      if (n==0) l
+      else l match {
+        case Nil => Nil
+        case Cons(h, t) => loop(t, n-1)
+      }
+    }
+    loop(l, n)
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    def loop(l: List[A], f: A => Boolean): List[A] = l match {
+      case Nil => Nil
+      case Cons(h, t) => if (f(h)) loop(t, f)
+                         else Cons(h, t)
+    }
+    loop(l, f)
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = {
+    def loop(l: List[A]) : List[A] = l match {
+      case Cons(h, t) => if (t==Nil) Nil
+                         else Cons(h, loop(t))
+    }
+    if (l == Nil) Nil
+    else loop(l)
+  }
 
   def length[A](l: List[A]): Int = sys.error("todo")
 
